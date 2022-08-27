72b43a08ff3ee83750368259e503ca3db71e8e22
zh <zh_01040@163.com>
2022-08-27 19:38:50 +0800

    react-router6 基础使用

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Outlet,
+  useParams,
+  useNavigate,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="product" element={<Product />}>
+              <Route path=":id" element={<ProductDetail />} />
+            </Route>
+          </Route>
+          <Route path="*" element={<NoMatch></NoMatch>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+function Layout() {
+  return (
+    <div>
+      <h1>Layout</h1>
+      <Link to="/">首页</Link>
+      <Link to="/product">商品</Link>
+      <Outlet></Outlet>
+    </div>
+  );
+}
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+function Product() {
+  return (
+    <div>
+      <h1>Product</h1>
+      <Link to="/product/123">详情</Link>
+      <Outlet></Outlet>
+    </div>
+  );
+}
+
+function ProductDetail() {
+  const params = useParams();
+  let navigate = useNavigate();
+  return (
+    <div>
+      <h1>ProductDetail:{params.id}</h1>
+      <button onClick={() => navigate("/")}>go home</button>
+    </div>
+  );
+}
+function NoMatch(params) {
+  return (
+    <div>
+      <h1>404</h1>
+    </div>
+  );
+}
